90fbd81509e1f03f7bee35e2148c26a527308889
KutuzovMishail <cutuzow.mihail2016@yandex.ru>
2022-10-12 17:40:07 +0300

    1 point

--- lab2.py
+++ lab2.py
@@ -1,1 +1,12 @@
 #Кутузов Михаил Андреевич Группа-3140 ИСУ-358729
+def color(code):
+    return f"\u001b[{code}m"
+White=color(47)
+Blue=color(44)
+end=color(0)
+for i in range(2):
+    print(White + '  '*10,Blue+'  '*5,White + '  '*15+end)
+for i in range(2):
+    print(Blue+'  '*31+end)
+for i in range(2):
+    print(White + '  '*10,Blue+'  '*5,White + '  '*15+end)
