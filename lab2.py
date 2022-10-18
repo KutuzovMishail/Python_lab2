987270ca51d6b3a0074be456ab2c2a1bf01535a8
KutuzovMishail <cutuzow.mihail2016@yandex.ru>
2022-10-18 22:11:21 +0300

    point 2

--- lab2.py
+++ lab2.py
@@ -2,11 +2,24 @@
 def color(code):
     return f"\u001b[{code}m"
 White=color(47)
+Black=color(40)
 Blue=color(44)
 end=color(0)
-for i in range(2):
-    print(White + '  '*10,Blue+'  '*5,White + '  '*15+end)
-for i in range(2):
-    print(Blue+'  '*31+end)
-for i in range(2):
-    print(White + '  '*10,Blue+'  '*5,White + '  '*15+end)
+#Flag
+for _ in range(2):
+    print(White + '  '*10+Blue+'  '*5+White + '  '*15+end)
+for _ in range(2):
+    print(Blue+'  '*30+end)
+for _ in range(2):
+    print(White + '  '*10+Blue+'  '*5+White + '  '*15+end)
+print()
+#pattern
+x=int(input())
+print(((White + '  '*3+White + ' '+Black + '  '*2+White + '  '*3+White + ' '+end)*2)*x)
+print(((White + '  '*2+White + ' '+Black + '  '*4+White + '  '*2+White + ' '+end)*2)*x)
+print(((White + '   '+ Black + '  '*6+White + '  '*1+White + ' '+end)*2)*x)
+print(((White + ' '+Black + '  '*8+White + ' '+end)*2)*x)
+print(((White + '   '+ Black + '  '*6+White + '  '*1+White + ' '+end)*2)*x)
+print(((White + '  '*2+White + ' '+Black + '  '*4+White + '  '*2+White + ' '+end)*2)*x)
+print(((White + '  '*3+White + ' '+Black + '  '*2+White + '  '*3+White + ' '+end)*2)*x)
+
