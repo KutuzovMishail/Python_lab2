acf4de2dc2277a3cc1da042cced5d4f2d928a5fb
KutuzovMishail <cutuzow.mihail2016@yandex.ru>
2022-10-18 23:00:07 +0300

    grafic

--- lab2.py
+++ lab2.py
@@ -23,3 +23,19 @@ print(((White + '   '+ Black + '  '*6+White + '  '*1+White + ' '+end)*2)*x)
 print(((White + '  '*2+White + ' '+Black + '  '*4+White + '  '*2+White + ' '+end)*2)*x)
 print(((White + '  '*3+White + ' '+Black + '  '*2+White + '  '*3+White + ' '+end)*2)*x)
 
+#grafic
+count=20
+count1=20
+for i in (0,2,4,6,8,12,14,16,18,20):
+    if count-i>10:
+        print(count-i, Black + '  '*(count1-2) + Blue+'    '+end,end='')
+    else:
+        print('',count-i, Black + '  ' * (count1 - 2) + Blue + '    ' + end, end='')
+    if count1-i<20:
+        print(Black+'  '*(20-count1)+end)
+    else:
+        print()
+    count1-=2
+print('   ', 0,'  ', 4,' ','', 8,'', 12,'',16,'',24,'',28,'',32,'',36,'',40)
+
+
